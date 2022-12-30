f01962a6bbb68e0efd2fde7a9dc2648118020a7a
sahanuser <sahan.user@gmail.com>
2022-12-30 17:52:03 +0530

    dmenu

--- pkg/asl-dmenu/opt/asl-dmenu/colors/asterlinux.h
+++
@@ -1,9 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#cccccc", "#282c34" },
-	[SchemeSel] = { "#000000", "#96cdfb" },
-	[SchemeSelHighlight] = { "#98be65", "#000000" },
-	[SchemeNormHighlight] = { "#98be65", "#000000" },
-	[SchemeOut] = { "#000000", "#96cdfb" },
-	[SchemeMid] = { "#d7d7d7", "#1c1f24" },
-};
--- pkg/asl-dmenu/opt/asl-dmenu/gitpush.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git add .
-
-git commit -m "dmenu"
-git push
